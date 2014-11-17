a5b49e63688b2f851de1f510c6b4abfc16f38412
App Academy Student <student@appacademy.io>
2014-11-17 15:45:56 -0800

    Finished listing all pokemon in Pokedex

--- skeleton/app/assets/javascripts/pokedex-1A.js
+++ skeleton/app/assets/javascripts/pokedex-1A.js
@@ -2,6 +2,7 @@ Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
   // create a list item with class of poke-list-item
   // Get naem and poke_type
   // Append result to this.$pokeList
+  console.log("adding to list")
   var $pokemonLi = $("<li></li>");
   $pokemonLi.append(pokemon.get("name") + " " + pokemon.get("poke_type"));
   $pokemonLi.addClass("poke-list-item");
@@ -9,4 +10,15 @@ Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
 };
 
 Pokedex.RootView.prototype.refreshPokemon = function (callback) {
+  var that = this;
+  this.pokes.fetch({
+    success: function() {
+      console.log("fetch successful");
+      that.pokes.each(that.addPokemonToList.bind(that));
+    },
+    
+    error: function(){
+    console.log("I am sorry Pokemon are not real");}  
+  });
+  
 };
