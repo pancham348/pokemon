a2ccbc18a564d1bdc5daa07e86cc78830743aa16
App Academy Student <student@appacademy.io>
2014-11-17 14:48:49 -0800

    Able to add pokemon to list

--- skeleton/app/assets/javascripts/pokedex-1A.js
+++ skeleton/app/assets/javascripts/pokedex-1A.js
@@ -1,4 +1,11 @@
 Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
+  // create a list item with class of poke-list-item
+  // Get naem and poke_type
+  // Append result to this.$pokeList
+  var $pokemonLi = $("<li></li>");
+  $pokemonLi.append(pokemon.get("name") + " " + pokemon.get("poke_type"));
+  $pokemonLi.addClass("poke-list-item");
+  this.$pokeList.append($pokemonLi);
 };
 
 Pokedex.RootView.prototype.refreshPokemon = function (callback) {
