ad4722452adea2a2f29de54e0839623afeb6eaeb
App Academy Student <student@appacademy.io>
2014-11-17 17:42:19 -0800

    Added uniqueness validations on the names of newly created pokemon

--- skeleton/app/assets/javascripts/pokedex-1C.js
+++ skeleton/app/assets/javascripts/pokedex-1C.js
@@ -4,11 +4,14 @@ Pokedex.RootView.prototype.createPokemon = function (attrs, callback) {
     // add pokemon to this.pokes
     //add the pokemon to the list
   // else we are all doomed
+  var that = this;
   var pokemon = new Pokedex.Models.Pokemon(attrs);
   pokemon.save(attrs,{
     success: function(){
       console.log("saved")
       console.log(pokemon.toJSON())
+      that.addPokemonToList(pokemon);
+      that.pokes.add(pokemon);
     },
     error: function(){
       console.log("Fool! man was not meant to play god!")
