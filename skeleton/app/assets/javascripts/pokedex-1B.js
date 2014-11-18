3426c4b57be72e24fea94c10851006870d36f695
App Academy Student <student@appacademy.io>
2014-11-17 16:00:11 -0800

    Added the ability to display images of a poken with renderPokemonDetail, with alt text

--- skeleton/app/assets/javascripts/pokedex-1B.js
+++ skeleton/app/assets/javascripts/pokedex-1B.js
@@ -1,5 +1,17 @@
 Pokedex.RootView.prototype.renderPokemonDetail = function (pokemon) {
+  // var $pokemonLi = $("<li></li>");
+ //  $pokemonLi.append(pokemon.get("name") + " " + pokemon.get("poke_type"));
+ //  $pokemonLi.addClass("poke-list-item");
+ //  this.$pokeList.append($pokemonLi);
+  var $pokemonDiv = $("<div></div>");
+  $pokemonDiv.addClass("detail");
+  var $pokemonImg = $("<img>").attr("src", pokemon.get("image_url")).attr(
+    "title", pokemon.get("name"));
+  $pokemonDiv.append($pokemonImg);
+  
+  this.$pokeDetail.append($pokemonDiv);
 };
 
 Pokedex.RootView.prototype.selectPokemonFromList = function (event) {
+  
 };
