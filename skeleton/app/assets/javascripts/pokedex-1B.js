ae2be6fcb0e119e7d2b1c5379fe2e17bc0d3e40a
App Academy Student <student@appacademy.io>
2014-11-17 16:42:55 -0800

    Completed rendering for pokemon details

--- skeleton/app/assets/javascripts/pokedex-1B.js
+++ skeleton/app/assets/javascripts/pokedex-1B.js
@@ -3,11 +3,19 @@ Pokedex.RootView.prototype.renderPokemonDetail = function (pokemon) {
  //  $pokemonLi.append(pokemon.get("name") + " " + pokemon.get("poke_type"));
  //  $pokemonLi.addClass("poke-list-item");
  //  this.$pokeList.append($pokemonLi);
+
   var $pokemonDiv = $("<div></div>");
   $pokemonDiv.addClass("detail");
   var $pokemonImg = $("<img>").attr("src", pokemon.get("image_url")).attr(
     "title", pokemon.get("name"));
   $pokemonDiv.append($pokemonImg);
+  
+  var keys = pokemon.keys();
+  for (var i = 0; i < keys.length; i++) {
+     if(keys[i] !=  "image_url"){
+       $pokemonDiv.append(keys[i] + ": " + pokemon.get(keys[i])).append("<br>");
+     }
+  }
   
   this.$pokeDetail.append($pokemonDiv);
 };
